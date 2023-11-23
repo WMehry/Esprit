d64928876626f422597ebf7d67f14711f7db4a6a
safa <safa.saoudi@esprit.tn>
2023-11-23 11:29:47 +0100

    routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResidencesComponent } from './residences/residences.component';
+import { FormResidenceComponent } from './form-residence/form-residence.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
+import { FormApartmentComponent } from './form-apartment/form-apartment.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'residences', component:ResidencesComponent},
+  {path:'addresidence', component:FormResidenceComponent},
+  {path:'detailsRsidence/:x', component:ApartmentsComponent},
+  {path:'addApartment/:id', component:FormApartmentComponent},
+  {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResidencesComponent } from './residences/residences.component';
+import { HeaderComponent } from './header/header.component';
+import { FormResidenceComponent } from './form-residence/form-residence.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
+import { FormApartmentComponent } from './form-apartment/form-apartment.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResidencesComponent
+    ResidencesComponent,
+    HeaderComponent,
+    FormResidenceComponent,
+    ApartmentsComponent,
+    FormApartmentComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
